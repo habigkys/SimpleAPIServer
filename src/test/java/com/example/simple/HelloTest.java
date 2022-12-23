0ba0f0e45f47d7efd3e7c7c72b7390cfeec3e2bc
김관주 <habigkys@evidnet.co.kr>
2022-12-23 11:01:31 +0900

    플러터 FCM 발송 구현
    Signed-off-by: 김관주 <habigkys@evidnet.co.kr>

--- src/test/java/com/example/simple/HelloTest.java
+++ src/test/java/com/example/simple/HelloTest.java
@@ -22,7 +22,7 @@ class HelloTest {
         String token = "eNTY2lzwRESScAPRIcFhjv:APA91bFe95HfYupF-QqJwpEuPfssF6xtco3I8-lDmfmnTVp0iseGpYOA1YOyQ6DHH4pLItW_kmA46Tz4aV1t5v_XTuw8vFPnq_3xvEi7xB8enJMKF9qwaP4gR3Udhb7-gJ69vMevfegG";
 
         // 단일 발송
-        String id = fcmService.singleSend(token, "연봉 동결", "연봉 동결");
+        String id = fcmService.singleSend(token, "스프링에서 알립니다", "귀하의 연봉은 동결됨");
 
         // 잔뜩 발송: 최대 500개 앱까지 동시에
 //        List<String> tokens = Lists.newArrayList();
